8d2297960f2269076df036a8e16e509207d7ab4e
Kealen-Pillay <kealenpillay@gmail.com>
2022-01-26 11:46:47 +1300

    Reworked appbar height

--- src/Components/DashboardAppbar.tsx
+++ src/Components/DashboardAppbar.tsx
@@ -15,7 +15,7 @@ interface Props {}
 const useStyles = makeStyles(() =>
   createStyles({
     root: {
-        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+      background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
     },
   })
 );
