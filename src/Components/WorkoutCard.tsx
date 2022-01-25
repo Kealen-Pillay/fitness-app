1742b53e63774cecfd9a1ad10ff9edf0b0b3a56a
Kealen-Pillay <kealenpillay@gmail.com>
2022-01-26 11:23:49 +1300

    Added Appbar component

--- src/Components/WorkoutCard.tsx
+++ src/Components/WorkoutCard.tsx
@@ -1,12 +1,46 @@
-import { createStyles, makeStyles } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { makeStyles, createStyles } from "@mui/styles";
+import MenuIcon from "@mui/icons-material/Menu";
 import React from "react";
 
 interface Props {}
 
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    },
+  })
+);
+
 export const WorkoutCard = (props: Props) => {
+  const classes = useStyles();
   return (
-    <div style={{ backgroundColor: "#F67F4C" }}>
-      <h1>Hello World</h1>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" className={classes.root}>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Home
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
