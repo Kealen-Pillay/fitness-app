88753f572c58712e5829ea1e1a6b938a01bb81b7
Kealen-Pillay <kealenpillay@gmail.com>
2022-02-06 12:51:54 +1300

    Changed document title

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Dashboard } from './Dashboard';
+import React, { useEffect } from "react";
+import "./App.css";
+import { Dashboard } from "./Dashboard";
 
 function App() {
+  useEffect(() => {
+    document.title = "Fitness Planner";
+  }, []);
+
   return (
-    <div style={{backgroundColor: "#1E1E1E"}}>
-    <Dashboard />
+    <div style={{ backgroundColor: "#1E1E1E" }}>
+      <Dashboard />
     </div>
   );
 }
